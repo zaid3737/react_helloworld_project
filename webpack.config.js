26861d98f92d8d88129f87e9f9bf40c48fa2b5f7
zaid3737 <mohammedzaidb7@gmail.com>
2025-07-30 16:00:57 +0530

    Initial React Hello World Commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js'
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '/'),
+    },
+    port: 7777,
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
